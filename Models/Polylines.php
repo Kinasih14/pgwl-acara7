2e62191eec68e87f2928934aa8155afaf9b1e4d8
Kinasih14 <142877930+Kinasih14@users.noreply.github.com>
2024-05-08 11:20:09 +0700

    Add data

---
+++ Models/Polylines.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Polylines extends Model
+{
+    use HasFactory;
+
+    protected $table = 'table_polylines';
+
+    protected $guarded = ['id'];
+
+    public function polylines()
+{
+    return $this->select(DB::raw('id, name, description, image, ST_AsGeoJSON(geom) as geom, created_at, updated_at'))->get();
+}
+
+}
